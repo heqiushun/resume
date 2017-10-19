0ac3251ec891e8895e2f6898079db4d69e9e363e
heqiushun <jidixueye@gmail.com>
2017-10-19 09:46:07 +0800

    personal8

--- js/hqs.js
+++ js/hqs.js
@@ -32,7 +32,7 @@ $(document).ready( function() {
 		{
 		  type: 'smallItem',
 		  label: '&nbsp;&nbsp;<span style="color:#dd6b66">基于药物小分子结构，构建预测不良反应模型</span>',
-		  shortContent: '通过拆分药物分子结构，以零散的结果片段作为描述符，挖掘其和药物不良反应的关系。主要负责：结构处理，数据分析和模型构建。',
+		  shortContent: '通过分解药物分子结构，以子片段作为描述符，挖掘其和药物不良反应的关系。主要负责：结构处理，数据分析和模型构建。',
 		  forcePosition: 'left'
 		},
 		{
@@ -44,7 +44,7 @@ $(document).ready( function() {
 		{
 		  type: 'smallItem',
 		  label: '<span style="color:#dd6b66">肺炎克雷伯氏菌SNP分型方法</span>&nbsp;&nbsp;',
-		  shortContent: '基于现有的多位点序列分型（MLST）方法，提出更快捷和方便的SNP分型方法，并简化和完善pipeline。',
+		  shortContent: '基于现有的多位点序列分型（MLST）方法，提出更快捷和方便的SNP分型方法，并简化和完善pipeline。主要负责：方法设计和优化，生成实验参考手册。',
 		  forcePosition: 'left'
 		},
 		{
@@ -56,7 +56,7 @@ $(document).ready( function() {
 		{
 		  type: 'smallItem',
 		  label: '<span style="color:#dd6b66">β-微管蛋白的氨基酸突变对微管动力学的影响</span>&nbsp;&nbsp;',
-		  shortContent: '模拟246位氨基酸突变前后β-微管蛋白构像的改变对αβ-微管蛋白的结合能力的影响，阐述了更稳定的αβ-微管蛋白会影响微管的动力学，进而影响拟南芥的生长。',
+		  shortContent: '模拟246位氨基酸突变前后，β-微管蛋白构像的改变对αβ-微管蛋白的结合能力的影响，阐述了更稳定的αβ-微管蛋白会影响微管的动力学，促进拟南芥的生长。',
 		  forcePosition: 'left'
 		}
 	  ]
@@ -75,7 +75,7 @@ $(document).ready( function() {
 		},
 		{
 		  type: 'smallItem',
-		  label: '&nbsp;&nbsp;<span style="color:#dd6b66">2015-2018&nbsp;&nbsp;研究生&nbsp;&nbsp;生物信息学</span>',
+		  label: '&nbsp;&nbsp;<span style="color:#dd6b66">2015-2018&nbsp;&nbsp;硕士&nbsp;&nbsp;生物信息学</span>',
 		  shortContent: '<span style="text-align:right">厦门大学<span>',
 		  fullContent: '<div style="text-align: center;"><img src="img/xmu.jpeg" style="width: 100%;" /></div>',
 		  showMore: '<a class="custom-show-btn-left"><i class="fa fa-plus-circle"></i></a>',
@@ -132,7 +132,7 @@ $(document).ready( function() {
     }	
 	//echarts
 	var myChart = echarts.init(document.getElementById('picture'),'vintage');
-	var dataArr = ['瞎逛', '煲剧', '美食', '阅读','瞎逛', '煲剧', '美食', '阅读','瞎逛', '煲剧', '美食', '阅读','瞎逛', '煲剧', '美食', '阅读','瞎逛', '煲剧', '美食', '阅读','瞎逛', '煲剧', '美食', '阅读','瞎逛', '煲剧', '美食', '阅读','瞎逛', '煲剧', '美食', '阅读','瞎逛', '煲剧', '美食', '阅读',]
+	var dataArr = ['瞎逛', '煲剧', '美食', '视频制作','阅读','瞎逛', '煲剧', '美食','视频制作', '阅读','瞎逛', '煲剧', '美食', '视频制作','阅读','瞎逛', '煲剧', '美食', '视频制作','阅读','瞎逛', '煲剧', '美食', '视频制作','阅读','瞎逛', '煲剧', '美食','视频制作', '阅读','瞎逛', '煲剧', '美食', '视频制作','阅读','瞎逛', '煲剧', '美食', '阅读','瞎逛', '煲剧', '美食', '视频制作','阅读',]
 	var data = [];
 	for (var i = 0; i < dataArr.length; ++i) {
 		 data.push({
